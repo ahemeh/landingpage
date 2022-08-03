c273aee74ee7ed4e4168677578b22baf616f4179
ahemeh <kristoperjensen123@gmail.com>
2022-08-03 22:56:26 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Landing Page</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top header">
+        <div class="container d-flex align-items-center justify-content-between">
+            <h1 class="logo"><a href="#hero">Home</a></h1>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="#about">About</a></li>
+                    <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+                    <li><a class="nav-link scrollto" href="#blog">Blog</a></li>
+                    <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
+            <h1 class="text-success">Hello ,Welcome To My official Website</h1>
+            <h2>Website Ini Menjelaskan Tentang Data Diri Saya dan Portfolio Project-Project Website yang Pernah Saya Kerjakan</h2>
+            <div class="d-flex" style="position: relative; top: 100px;">
+                <a href="#about" class="nav-link scrollto">
+                    <h1><i class='bx bx-chevrons-down bx-fade-down'></i></h1>
+                </a>
+            </div>
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
+            <div class="container">
+                <div class="row">
+                    <div class="text-center mb-5">
+                        <h1 class="text-success">About Me</h1>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="text-center">
+                            <img src="images/avatar5.png" class="img-circle" alt="">
+                        </div>
+                        <div class="text-center mr-3 mt-3">
+                            <h5 class="mr-3">Junior Web Developer</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h2>Kristoper Jensen, S. Kom</h2>
+                        <label for="">Halo, Saya Kristoper Jensen Saya lahir di Serukam 4 Juli 1999, Saya berasal dari Samalantan, Kabupaten Bengkayang, Provinsi Kalimantan Barat, Saya Sangat Senang Belajar Mengenai Teknologi dan Saya Ingin Menjadi Seorang Web Developer.</label>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End About Section -->
+
+        <!-- ======= Portfolio Section ======= -->
+        <section id="portfolio" class="portfolio" style="background-color: #91da324f;">
+            <div class="container">
+                <div class="row">
+                    <div class="text-center mb-5">
+                        <h1 class="text-success">Portfolio</h1>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/Screenshot (4).png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/Screenshot (10).png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/dekranasda.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/penjual dekranasda.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/Screenshot rujakita.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/admin rujakita.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/bjsape.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/produk bjsape.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app mb-3">
+                        <img src="images/video bjsape.png" class="img-fluid" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </section><!-- End Portfolio Section -->
+
+        <section id="blog" class="blog">
+            <div class="container">
+                <div class="row">
+                    <div class="text-center">
+                        <h1 class="text-success">Blog</h1>
+                    </div>
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                            <div class="card">
+                                <img src="images/images.jpg" class="card-img-top" height="250" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Manajemen Waktu</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-success">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <img src="images/jaringan.jpg" class="card-img-top" height="250" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Jaringan Internet</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-success">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <img src="images/processor.jpg" class="card-img-top" height="250" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mengenal Perangkat Lunak dan Perangkat Keras</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-success">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <img src="images/security.jpg" class="card-img-top" height="250" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mengenal Cyber Security</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-success">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="team" class="team">
+            <div class="container">
+                <div class="text-center mb-5">
+                    <h1 class="text-success font-weight-bold">My Team</h1>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="member">
+                            <div class="pic"><img src="images/avatar5.png" class="img-fluid" width="300" alt=""></div>
+                            <div class="team-info">
+                                <h4>Kristoper Jensen</h4>
+                                <span>Chief Executive Officer</span>
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="member">
+                            <div class="pic"><img src="images/avatar04.png" class="img-fluid" width="300" alt=""></div>
+                            <div class="team-info">
+                                <h4>Andy</h4>
+                                <span>Web Developer</span>
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="member">
+                            <div class="pic"><img src="images/avatar2.png" class="img-fluid" width="300" alt=""></div>
+                            <div class="team-info">
+                                <h4>Cindy</h4>
+                                <span>Digital Marketing</span>
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact produk">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+                    <p id="kontak">Contact Us</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="g-maps">
+                                    <iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d646.707193434775!2d109.19826676028502!3d0.7941341373317452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31e33e1af536226f%3A0x34d6e60ae42c99e!2sJl.%20Pasar%20Samalantan%2C%20Marunsu%2C%20Samalantan%2C%20Kabupaten%20Bengkayang%2C%20Kalimantan%20Barat%2079281!5e0!3m2!1sid!2sid!4v1644047533893!5m2!1sid!2sid" height="380" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-4 mt-lg-0">
+                        <form action="forms/kirim.php" method="post" role="form" class="php-email-form">
+                            <div class="row">
+                                <div class="col-md-6 form-group">
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                                </div>
+                                <div class="col-md-6 form-group mt-3 mt-md-0">
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                                </div>
+                            </div>
+                            <div class="form-group mt-3">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                            </div>
+                            <div class="form-group mt-3">
+                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                            </div>
+                            <div class="my-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Your message has been sent. Thank you!</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Contact Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-info">
+                            <h4>Hubungi Kami</h4>
+                            <p>
+
+                            <div class="contact">
+                                <i class='bx bxl-whatsapp'>: <a href="https://api.whatsapp.com/send?phone=+6289624112411&text=Halo%20Saya%20Kristoper%20ingin%20bertanya%20sesuatu"> 089624112411</a></i><br>
+                                <i class='bx bx-mail-send'>: <a href="#">kristoperjensen123@gmail.com</a></i>
+                            </div>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <h4>Media Sosial</h4>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="" class="youtube"><i class='bx bxl-youtube'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
